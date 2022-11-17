42eb5d9c1670350298118d228d2d0748558627e1
Maimoona M.Irfan <87384658+maimoonairfan@users.noreply.github.com>
2022-11-17 19:52:54 +0500

    Add files via upload

---
+++ Assignment3.dart
@@ -0,0 +1,76 @@
+
+void main(){
+// Answer to Question1
+// List name = ['john','joe','oliver'];
+// print("The list of names\n$name");
+// Answer to Question2
+// List days = [];
+// days.add('Saturday');
+// days.add('Sunday');
+// days.add('Monday');
+// days.add('Tuesday');
+// days.add('Wednesday');
+// days.add('Thursday');
+// days.add('Friday');
+// print("The days in a week:\n$days");
+
+// Answer to Question2
+  // List<String> days = [
+  //   "Sunday",
+  //   "Monday",
+  //   "Tuesday",
+  //   "Wednesday",
+  //   "Thursday",
+  //   "Friday",
+  //   "Saturday"
+  // ];
+
+  // List<String> startWithS =
+  //     days.where((element) => element.startsWith("S")).toList();
+
+  // print(startWithS);
+// Answer to Question 3
+// List friends_name = ['Deepa','Isha','Nida','Shajia'];
+// if(friends_name==['D']){
+// print("Exist");
+// }
+// else{
+//   print("Not Exist");
+// }
+// Answer to Question4
+// Map x = {'name':'Maimona','address':'FB Area','age': 20,'country':'Pakistan'};
+// print(x);
+// x.remove('country');
+// print(x);
+// x.addAll({'country':'USA'});
+// print(x);
+// Answer to Question 5
+// List data = [{'name':'Joe','phone':12456,'age': 20,'country':'Pakistan'}];
+// for(var i=0; i<data.length;i++){
+//   if(data[i].length == 4){
+//     print(data[i]);
+//   }
+// };
+
+// Answer to Question 7
+// Map <String, double> mathMarks = {
+//   'ram': 30,
+//   'mark': 32,
+//   'harry': 88,
+//   'raj': 69,
+//   'john': 15,
+// };
+// mathMarks.remove((key,value) => value <=30);
+// print(mathMarks); 
+// Answer to Question 8
+// Map<String, double> expenses = {
+//   'sun': 3000.0,
+//   'mon': 3000.0,
+//   'tue': 3234.0,
+// };
+// expenses.putIfAbsent('fri', () => 456);
+// print(expenses);
+// 
+
+  
+}
